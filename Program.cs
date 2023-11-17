639c303bbc22c255bb93cdc1c4f0d1f75f515cf3
michele <105759846+Michelepereira07@users.noreply.github.com>
2023-11-17 16:46:44 -0300

    Exemplo de uma lista de alunos

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using ExemploExplorando.Models;
+
+Pessoa p1 = new Pessoa(nome: "Carlos", sobrenome: "Silva");
+Pessoa p2 = new Pessoa(nome: "Michele", sobrenome: "Pereira");
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
+
